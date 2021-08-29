5926ab66ef47a1d23aeac1ae852dd51ca867ed5a
sharbellkhell <88714465+sharbellkhell@users.noreply.github.com>
2021-08-29 22:03:10 +0300

    Update examDetails.h

--- part_a/examDetails.h
+++ part_a/examDetails.h
@@ -4,30 +4,36 @@
 #include <string>
 #include <exception>
 #include <iostream>
+#include <cmath>
+
+using std::endl;
+using std::exception;
+using std::ostream;
 
 namespace mtm{
-    class ExamDetails{
-    public:
-        ExamDetais(int course_number, unsigned int month, unsigned int day,
-            double hour, unsigned int duration, std::string zoom_link);
 
-        class IndalidDateException : public std::exception;
-        class IndalidTimeException : public std::exception;
-        class IndalidArgsException : public std::exception;
+    class ExamDetails{  
+    public:
+        ExamDetails(int course_number, unsigned int month, unsigned int day,
+            double hour, unsigned int duration, std::string zoom_link = "");
+
+        class InvalidDateException : public std::exception {};
+        class InvalidTimeException : public std::exception {};
 
         ~ExamDetails() noexcept;
         ExamDetails(const ExamDetails& source);
         ExamDetails& operator=(const ExamDetails& source);
         const std::string& getLink() const;
         void setLink(const std::string& new_link);
-
-        static constexpr unsigned int DAYS_IN_MONTH = 30;
         int operator-(const ExamDetails& other) const;
-        bool operator<(const ExamDetails& other) const;
-        friend ostream& operator<<(ostream& os, const ExamDetails&);
-    
-        static ExamDetails makeMatamExam();  
-    };
+        friend ostream& operator<<(ostream& os, const ExamDetails& details);
+        static ExamDetails makeMatamExam(); 
+
+        static constexpr unsigned int MIN_MONTH = 1;
+        static constexpr unsigned int MAX_MONTH = 12;
+        static constexpr unsigned int MIN_DAY = 1;
+        static constexpr unsigned int MAX_DAY = 30;
+        static constexpr double HALF_HOUR = 0.5 ;
 
     private:
         int course_number;
@@ -37,7 +43,21 @@ namespace mtm{
         unsigned int duration;
         std::string zoom_link;
 
-    static std::string hourToString(double hour);
+        static std::string hourToString(double hour);
+    };
+
+    bool operator<(const ExamDetails& first, const ExamDetails& second);
+
+    ostream& operator<<(ostream& os, const ExamDetails& details)
+    { //unable to place func outside header file
+        os << "Course Number: " <<  details.course_number << endl;
+        os << "Time: " << details.day << "." << details.month << " at " 
+        << ExamDetails::hourToString(details.hour) << endl; 
+
+        os << "Duration: " << ExamDetails::hourToString((double)details.duration) << endl; 
+        os << "Zoom Link: " << details.zoom_link << endl;
+        return os;
+    }
 }
 
 #endif
