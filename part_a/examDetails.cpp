56c046edc71650135daee0ebb7ff8a7328138d89
sharbellkhell <88714465+sharbellkhell@users.noreply.github.com>
2021-08-29 22:02:15 +0300

    Update examDetails.cpp

--- part_a/examDetails.cpp
+++ part_a/examDetails.cpp
@@ -3,20 +3,28 @@
 
 using namespace mtm;
 
-ExamDetais::ExamDetais(int course_number, unsigned int month, unsigned int day,
-    double hour, unsigned int duration, std::string zoom_link): course_number(course_number),
-    month(month), day(day), duration(duration), zoom_link(zoom_link) {
-    if(course_number < 0){
-        throw ExamDetais::InvalidArgsException();
+static bool isValidHour(double hour){
+    if(hour < 0){
+        return false;
     }
-    if(month < 1 || month > 12 || day < 1 || day > 30){
-        throw ExamDetais::InvalidTimeException();
+    double intpart;
+    if( std::modf(hour, &intpart) != ExamDetails::HALF_HOUR ){
+        return false;
     }
+    return true;
+        
 }
 
-class ExamDetails::IndalidDateException{};
-class ExamDetails::IndalidTimeException{};
-class ExamDetails::IndalidArgsException{};
+ExamDetails::ExamDetails(int course_number, unsigned int month, unsigned int day,
+    double hour, unsigned int duration, std::string zoom_link): course_number(course_number),
+    month(month), day(day), duration(duration), zoom_link(zoom_link) {
+    if(month < MIN_MONTH || month > MAX_MONTH || day < MIN_DAY || day > MAX_DAY){
+        throw InvalidDateException();
+    }
+    if(!isValidHour(hour) ){
+        throw InvalidTimeException();
+    }
+}
 
 ExamDetails::~ExamDetails(){}
 
@@ -24,12 +32,15 @@ ExamDetails::ExamDetails(const ExamDetails& source): course_number(source.course
     month(source.month), day(source.day), duration(source.duration), zoom_link(source.zoom_link) {}
 
 ExamDetails& ExamDetails::operator=(const ExamDetails& source){
+    if (this == &source){
+        return *this;
+    }
     course_number = source.course_number;
     month = source.month;
     day = source.day;
     duration = source.duration;
     zoom_link = source.zoom_link;
-    return this;
+    return *this;
 }
 
 const std::string& ExamDetails::getLink() const{
@@ -42,27 +53,21 @@ void ExamDetails::setLink(const std::string& new_link){
 
 int ExamDetails::operator-(const ExamDetails& other) const{ 
     //if we are not in the same year we might have an issue :(
-    return DAYS_IN_MONTH*(month - other.month) + day - other.day;
+    return MAX_DAY*(month - other.month) + day - other.day;
 }
 
-bool ExamDetails::operator<(const ExamDetails& other) const{
-    return *this - other < 0;
+bool operator<(const ExamDetails& first, const ExamDetails& second){ //changed to non-member function
+    return first-second < 0;
 }
 
-ostream& operator<<(ostream& os, const ExamDetails& details){
-    os << "Course Number: " <<  details.course_number << endl;
-    os << "Time: " << details.day << "." << details.month << " at " 
-        << ExamDetails::hourToString(details.hour) << endl; 
-    os << "Duration: " ExamDetails::hourToString(details.duration) << endl; 
-    os << "Zoom Link: " << details.zoom_link << endl;
-    return os;
+
+
+
+ExamDetails ExamDetails::makeMatamExam(){
+    return ExamDetails(234124, 7, 28, 13, 3, "https://tinyurl.com/59hzps6m");
 }
 
-static ExamDetails ExamDetails::makeMatamExam(){
-    return ExamDetails(234124, 7, 28, 13, 3, "59hzps6m/com.tinyurl://h");
-}
-
-static std::string ExamDetails::hourToString(double hour){
+std::string ExamDetails::hourToString(double hour){
     unsigned int hour_halfs = std::nearbyint(hour*2);
     std::string result = std::to_string(hour_halfs/2);
     result += ":";
